820929350c7e923372406f9ccd73614773ffb8a2
AlexPotemka <funkymaloy@gmail.com>
2024-03-17 16:57:22 +0500

    v2.4.0 - second stage transforms

---
+++ UXFeedbackSDK.xcframework/ios-arm64_x86_64-simulator/UXFeedbackSDK.framework/Headers/UXFeedbackSDK.h
@@ -0,0 +1,21 @@
+//
+//  UXFeedbackSDK.h
+//  UXFeedbackSDK
+//
+//  Created by Dmitry Kudryavtsev on 09/05/2019.
+//  Copyright © 2019 UXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <UXFeedbackSDK/YYImage.h>
+
+//! Project version number for UXFeedbackSDK.
+FOUNDATION_EXPORT double UXFeedbackSDKVersionNumber;
+
+//! Project version string for UXFeedbackSDK.
+FOUNDATION_EXPORT const unsigned char UXFeedbackSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UXFeedbackSDK/PublicHeader.h>
+
+
